c945411a87b0297975ae93307795675a3f33f5aa
Prashant Mishra <prashantmishra111@outlook.com>
2023-06-03 00:24:22 +0530

    dp check with cc

--- src/components/pages/messages/ChatComp.js
+++ src/components/pages/messages/ChatComp.js
@@ -1,7 +1,49 @@
 import React from "react";
 import styles from "../../../style/Messages.module.scss";
+import note from "../../../assets/note.png";
+import _picon from "../../../assets/_picon.jpg";
+
 function ChatComp() {
-  return <div>ChatComp this is Message Component!</div>;
+  return (
+    <div className={styles.__chat_container}>
+      <div className={styles.__chat_header}>
+        <div className={styles.__chat_header_left}>
+          <img src={_picon} alt="profile icon" />
+          <h3>prashant8459</h3>
+        </div>
+        <div className={styles.__chat_header_right}>
+          <img src={note} alt="icon" />
+          <img src={note} alt="icon" />
+          <img src={note} alt="icon" />
+        </div>
+      </div>
+      <div className={styles.__chat_body}>
+        <div className={styles.__chat_body_left}>
+          <div className={styles.__chat_body_left_message}>
+            <img src={_picon} alt="profile icon" />
+            <p>hello</p>
+          </div>
+        </div>
+        <div className={styles.__chat_body_right}>
+          <div className={styles.__chat_body_right_message}>
+            <img src={_picon} alt="profile icon" />
+            <p>hello</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.__chat_footer}>
+        <div className={styles.__chat_footer_left}>
+          <img src={note} alt="icon" />
+          <img src={note} alt="icon" />
+          <img src={note} alt="icon" />
+        </div>
+        <div className={styles.__chat_footer_right}>
+          <input type="text" placeholder="message" />
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatComp;
